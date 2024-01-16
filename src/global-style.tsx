40d081a9b047d9fea63a29b76a859c638214bfa6
Vicky Kumar <vicky.k@muvin.in>
2024-01-16 10:05:42 +0530

    feat: added styled component and basic css reset

---
+++ src/global-style.tsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    
+    *, *::before, *::after {
+        box-sizing: border-box;
+    }
+    * {
+        margin: 0;
+    }
+
+    body {
+        line-height: 1.5;
+        -webkit-font-smoothing: antialiased;
+        font-family: 'Encode Sans', sans-serif;
+    }
+
+    img, picture, video, canvas, svg {
+        display: block;
+        max-width: 100%;
+    }
+
+    input, button, textarea, select {
+        font: inherit;
+    }
+
+    p, h1, h2, h3, h4, h5, h6 {
+        overflow-wrap: break-word;
+    }
+
+    #root, #__next {
+        isolation: isolate;
+    }
+    :root{
+        --star-rating:"#FFB100";
+        --text:"#1D2024";
+        --orange:"#FF8B00";
+        --yellow: "#FFD200";
+        --text-light:"#7E8994";
+        --red: "#D80A00";
+        --green:"#108510";
+        --black:"#000000";
+        --white:"#FFFFFF";
+        --bg-card:"#F0F1F2";
+
+    }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import { GlobalStyle } from "./global-style";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
